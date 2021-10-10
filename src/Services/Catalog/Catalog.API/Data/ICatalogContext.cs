2859746d41cbed4e19e96557791417fd063ac56d
Hunaid Hussain <Hunaid.Hussain@DuffandPhelps.com>
2021-10-11 02:33:53 +0530

    Created Product Repositories

--- src/Services/Catalog/Catalog.API/Data/ICatalogContext.cs
+++ src/Services/Catalog/Catalog.API/Data/ICatalogContext.cs
@@ -5,9 +5,8 @@ namespace Catalog.API.Data
 {
     public interface ICatalogContext
     {
-        public interface ICatalogContext
-        {
-            IMongoCollection<Product> Products { get; }
-        }
+       
+           IMongoCollection<Product> Products { get; }
+       
     }
 }
